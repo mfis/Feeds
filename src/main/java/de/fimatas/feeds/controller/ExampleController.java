25e002013d85bb92e5af66620161ddcbc9e453fb
mfis <fischermatthias1980@googlemail.com>
2024-02-04 15:05:03 +0100

    example links

--- src/main/java/de/fimatas/feeds/controller/ExampleController.java
+++ src/main/java/de/fimatas/feeds/controller/ExampleController.java
@@ -7,10 +7,12 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
+import java.util.Date;
 
 @Controller
 @RequestMapping("/example")
@@ -29,12 +31,16 @@ public class ExampleController {
 
         SyndEntry entry1 = new SyndEntryImpl();
         entry1.setTitle("Example title 1");
+        entry1.setLink("https://feeds.fimatas.de/example/1");
+        entry1.setPublishedDate(new Date());
         var content1 = new SyndContentImpl();
         content1.setValue("This is content1");
         entry1.setDescription(content1);
 
         SyndEntry entry2 = new SyndEntryImpl();
         entry2.setTitle("Example title 2");
+        entry1.setLink("https://feeds.fimatas.de/example/2");
+        entry2.setPublishedDate(new Date());
         var content2 = new SyndContentImpl();
         content2.setValue("This is content2");
         entry2.setDescription(content2);
@@ -49,4 +55,12 @@ public class ExampleController {
         response.getWriter().print(new SyndFeedOutput().outputString(feed));
     }
 
+    @GetMapping("/{key}")
+    @ResponseBody
+    public String getEntry(@PathVariable String key) {
+        if (key.equals("1") || key.equals("2")) {
+            return "--" + key + "--";
+        }
+        return "--unknown--";
+    }
 }
