6d194ac3c9ffba23fedeec03d7d0cfd8e60daaf0
mfis <fischermatthias1980@googlemail.com>
2025-02-28 17:23:05 +0100

    healthcheck

--- src/main/java/de/fimatas/feeds/controller/RootController.java
+++ src/main/java/de/fimatas/feeds/controller/RootController.java
@@ -1,5 +1,7 @@
 package de.fimatas.feeds.controller;
 
+import de.fimatas.feeds.model.FeedsCache;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,5 +15,15 @@ public class RootController {
     @GetMapping("/")
     public String root() {
         return "redirect:" + rootRedirectUrl;
+    }
+
+
+    @GetMapping("api/healthcheck")
+    public void healthcheck(HttpServletResponse response) {
+        if(FeedsCache.getInstance().isNotValid()){
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }else{
+            response.setStatus(HttpServletResponse.SC_OK);
+        }
     }
 }
